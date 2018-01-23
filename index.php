5c0d81babb3c0ef25061e3ae99dfdd7e09f94a50
Neto <contato@netosales.com.br>
2018-01-23 13:00:02 -0200

    Com arquivo html funciona.

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <?php 
 
 require_once("vendor/autoload.php");
+use \Slim\Slim;
+use \Hcode\Page;
+
 
 $app = new \Slim\Slim();
 
+$app = new Slim();
 $app->config('debug', true);
 
 $app->get('/', function() {
+$page = new Page();
+$page->setTpl("index");
     
-	echo "OK";
-	$sql = new Hcode\DB\Sql();
-	$results = $sql->select("SELECT * FROM tb_users");
-	echo json_encode($results);
+	//echo "OK";
+	//$sql = new Hcode\DB\Sql();
+	//$results = $sql->select("SELECT * FROM tb_users");
+	//echo json_encode($results);
 
 });
 
